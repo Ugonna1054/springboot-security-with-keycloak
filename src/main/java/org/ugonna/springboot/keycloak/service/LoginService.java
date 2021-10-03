d36463bd1de21ec86251fd5aa5402b0f5ce85f7d
Ugonna Ubaka <Ugonna.Ubaka@stanbicibtc.com>
2021-10-03 09:55:08 +0100

    init repo

--- src/main/java/org/ugonna/springboot/keycloak/service/LoginService.java
+++ src/main/java/org/ugonna/springboot/keycloak/service/LoginService.java
@@ -1,23 +1,18 @@
 package org.ugonna.springboot.keycloak.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ServerErrorException;
 import org.ugonna.springboot.keycloak.dto.LoginRequest;
 import org.ugonna.springboot.keycloak.dto.LoginResponse;
 
 
 @Service
 public class LoginService {
-
 
     @Value("${app.keycloak.login.url}")
     private String loginUrl;
